faef0a03a1416d24c6e51c1fb319aa0b51fa0e05
Ali El-esawy <73000699+alielesawy@users.noreply.github.com>
2024-09-27 19:22:09 +0000

    try agin

---
+++ tina/config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from 'tinacms'
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main'
+
+export default defineConfig({
+	branch: 'main',
+	clientId: '70b90eda-b65f-4e0f-a48a-7732dff54275', // Get this from tina.io
+	token: '296c0c4ac3fd2bbbafb4bc91fa1363ee7c3229e7', // Get this from tina.io
+
+	build: {
+		outputFolder: 'admin',
+		publicFolder: 'public'
+	},
+	media: {
+		tina: {
+			mediaRoot: '',
+			publicFolder: 'public'
+		}
+	},
+	schema: {
+		collections: [
+			{
+				name: 'post',
+				label: 'Posts',
+				path: 'content/posts',
+				fields: [
+					{
+						type: 'string',
+						name: 'title',
+						label: 'Title',
+						isTitle: true,
+						required: true
+					},
+					{
+						type: 'rich-text',
+						name: 'body',
+						label: 'Body',
+						isBody: true
+					}
+				]
+			}
+		]
+	}
+})
